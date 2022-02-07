461fc1b4611670fbba44751b061bd6ad9633624b
chenkunbo <kikichen6677@gmail.com>
2022-02-07 00:02:06 -0800

    Week-1 & 2 assignment

---
+++ Strings, Arrays, Loops and Class Assignment/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    /// region Assignment Code
+    // Question 1: Write a program find if String is Palindrome?
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ///endregion
+}
