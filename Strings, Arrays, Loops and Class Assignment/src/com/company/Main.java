6520e19e8a8fbcbe6ba31b0656f5997f92e8bf30
chenkunbo <kikichen6677@gmail.com>
2022-02-08 14:29:32 -0800

    Week-1 & 2 assignment

--- Strings, Arrays, Loops and Class Assignment/src/com/company/Main.java
+++ Strings, Arrays, Loops and Class Assignment/src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -22,22 +25,36 @@ public class Main {
             end --;
         }
         if(isPalindrome){
-            System.out.println("Palindrome");;
+            System.out.println("Question 1: Write a program find if String is Palindrome?");
+            System.out.println("Palindrome");
         }
         else{
+            System.out.println("Question 1: Write a program find if String is Palindrome?");
             System.out.println("Not a Palindrome");
         }
+
+        // Question2: Write a program to check if a string has all unique characters.
+        String str1 = "abcdef89012";
+        System.out.println("Question2: Write a program to check if a string has all unique characters.");
+        System.out.println(hasUniqueChars(str1));
+
+
 
     }
 
 
-
-
-
-
-
-    /// region Assignment Code
-    // Question 1: Write a program find if String is Palindrome?
+    /// region assignment code
+    // Question2: Write a program to check if a string has all unique characters.
+    private static boolean hasUniqueChars(String str){
+        Set<Character> set = new HashSet<>();
+        for(char c : str.toCharArray()){
+            if(set.contains(c)){
+                return false;
+            }
+            set.add(c);
+        }
+        return true;
+    }
 
 
 
