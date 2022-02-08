622c1a18ec24bffc203a9f75e1fbc360f56cb37a
chenkunbo <kikichen6677@gmail.com>
2022-02-07 18:17:26 -0800

    Week-1 & 2 assignment

--- Strings, Arrays, Loops and Class Assignment/src/com/company/Main.java
+++ Strings, Arrays, Loops and Class Assignment/src/com/company/Main.java
@@ -1,13 +1,37 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
+        // Question 1: Write a program find if String is Palindrome?
+        String str = "intni";
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while(start < end){
+            if (str.charAt(end) != str.charAt(start)) {
+                isPalindrome = false;
+                break;
+            }
+            start ++;
+            end --;
+        }
+        if(isPalindrome){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not a Palindrome");
+        }
     }
 
     /// region Assignment Code
     // Question 1: Write a program find if String is Palindrome?
+
 
 
 
