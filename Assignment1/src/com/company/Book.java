f87a8c18d338259476ab1f6c25ec9e6e91d81474
chenkunbo <kikichen6677@gmail.com>
2022-02-09 20:52:14 -0800

    Week-1 & 2 assignment: Strings, Arrays, Loops and Class

---
+++ Assignment1/src/com/company/Book.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Book {
+    // private variables
+    String _name;
+    int _bookID;
+    String _genres;
+    double _price;
+
+    // constructor
+    public Book(String name, int bookID, String genres, double price){
+        _name = name;
+        _bookID = bookID;
+        _genres = genres;
+        _price = price;
+    }
+    // getters
+    public String getName(){ return _name;}
+    public int getBookID(){ return _bookID;}
+    public String getGenres(){ return _genres;}
+    public double getPrice(){ return _price;}
+
+    // setters
+    public void setName(String name){_name = name;}
+    public void setBookID(int bookID){_bookID = bookID;}
+    public void setGenres(String genres){_genres = genres;}
+    public void setPrice(double price){_price = price;}
+
+}
