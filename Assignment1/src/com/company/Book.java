7dd3ff01786ef3cba63ea3afbad07fb796ab6ece
chenkunbo <kikichen6677@gmail.com>
2022-02-09 22:04:52 -0800

    Week-1 & 2 assignment: Strings, Arrays, Loops and Class

--- Assignment1/src/com/company/Book.java
+++ Assignment1/src/com/company/Book.java
@@ -32,7 +32,7 @@ public class Book {
     public void setGenres(String genres){_genres = genres;}
     public void setPrice(double price){_price = price;}
 
-    public String toString(){
+    public Book(){
         return "Name: " + _name + "\n" + "BookID: " + _bookID + "\n" +
                 "Genres: " + _genres + "\n" + "Price: " + _price + "\n";
     }
