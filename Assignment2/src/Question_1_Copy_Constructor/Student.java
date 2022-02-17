6318dc31c0fe3ecab47954c80f52fd63c1752c7e
chenkunbo <kikichen6677@gmail.com>
2022-02-17 00:06:47 -0800

    Week-3 assignment: Copy constructor, Comparator, Classes

---
+++ Assignment2/src/Question_1_Copy_Constructor/Student.java
@@ -0,0 +1,35 @@
+package Question_1_Copy_Constructor;
+
+public class Student {
+    // Question1: Implement given program using copy constructor by creating a class Student with two parameter
+    // 1. int rollNumber; 2. String name
+    // Class Main {
+    //     public static void main(String[] args) {
+    //          Student student1 = new Student(101, "ABC");
+    //          system.out.println(" Roll number of the first student: "+ student1.printRoll());
+    //          system.out.println(" Name of the first student: "+ student1.printName());
+    //          Student student2 = new Student(student1);
+    //          system.out.println(" Roll number of the second student: "+     student2.printRoll());
+    //          system.out.println(" Name of the second student: "+ student2.printName());
+    //          }
+    //     }
+
+    int rollNumber;
+    String name;
+
+    public Student(int _rollNumber, String _name){
+        rollNumber = _rollNumber;
+        name = _name;
+
+    }
+    public Student(Student student){
+        rollNumber = student.rollNumber;
+        name = student.name;
+    }
+
+
+
+
+
+
+}
--- Assignment2/src/com/company/Main.java
+++ Assignment2/src/com/company/Main.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import Question_1_Copy_Constructor.Student;
+
+import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        // Question1: Implement given program using copy constructor by creating a class Student with two parameter
+        // 1. int rollNumber; 2. String name
 
+        Student student1 = new Student(101, "ABC");
+        System.out.println(" Roll number of the first student: "+ student1.printRoll());
+        System.out.println(" Name of the first student: "+ student1.printName());
+        Student student2 = new Student(student1);
+        System.out.println(" Roll number of the second student: "+     student2.printRoll());
+        System.out.println(" Name of the second student: "+ student2.printName());
 
     }
 }
+
