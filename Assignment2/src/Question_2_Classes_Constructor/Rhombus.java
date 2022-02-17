364071883e1ea6971e546526003a770113fcbe63
chenkunbo <kikichen6677@gmail.com>
2022-02-17 02:15:27 -0800

    Week-3 assignment: Copy constructor, Comparator, Classes

--- Assignment2/src/Question_2_Classes_Constructor/Rhombus.java
+++ Assignment2/src/Question_2_Classes_Constructor/Rhombus.java
@@ -1,6 +1,18 @@
 package Question_2_Classes_Constructor;
 
-public class Rhombus {
+public class Rhombus extends Shape{
+
+    // 1. Fields: diagonal d1, diagonal d2
+    public double diagonal_d1;
+    public double diagonal_d2;
+
+    // 2. Constructor
+    public Rhombus(String name, String color, double area, double perimeter, double diagonal_d1, double diagonal_d2) {
+        super(name, color, area, perimeter);
+        this.diagonal_d1 = diagonal_d1;
+        this.diagonal_d2 = diagonal_d2;
+    }
+
 
 
 
