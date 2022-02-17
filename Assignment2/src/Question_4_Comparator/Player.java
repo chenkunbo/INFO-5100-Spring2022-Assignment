61fb1af203f1680b1224e544d5390f2165274cf7
chenkunbo <kikichen6677@gmail.com>
2022-02-17 04:28:05 -0800

    Week-3 assignment: Copy constructor, Comparator, Classes

--- Assignment2/src/Question_4_Comparator/Player.java
+++ Assignment2/src/Question_4_Comparator/Player.java
@@ -1,6 +1,8 @@
 package Question_4_Comparator;
 
-public class Player {
+import java.util.Comparator;
+
+public class Player implements Comparator<Player> {
     //Question 4: Comparators. Create a comparator and use it to sort an array.
     // Create Player class. It has 2 fields: a String name and a score integer.
     // Given an array of Player objects, write a comparator that sorts them in order of decreasing score;
@@ -16,12 +18,15 @@ public class Player {
 
     }
 
-
-    public int compareTo(Player o){
-
-
-        return 0;
+    public int compareTo(Player a, Player b){
+        if(a.score == b.score){
+            return a.name.compareTo((b.name));
+        }
+        return b.score - a.score;
     }
 
-
+    @Override
+    public int compare(Player o1, Player o2) {
+        return 0;
+    }
 }
