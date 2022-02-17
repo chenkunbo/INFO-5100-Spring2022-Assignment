41bb627662396bc910f3d8deb57f3d356fbb6522
chenkunbo <kikichen6677@gmail.com>
2022-02-17 01:15:00 -0800

    Week-3 assignment: Copy constructor, Comparator, Classes

--- Assignment2/src/Question_2_Classes_Constructor/Shape.java
+++ Assignment2/src/Question_2_Classes_Constructor/Shape.java
@@ -2,11 +2,40 @@ package Question_2_Classes_Constructor;
 
 public class Shape {
 
+    // 1. Fields: name, color, area, perimeter
     public String name;
     public String color;
     public double area;
     public double perimeter;
 
+    // 2. Constructors
+    public Shape(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
+    public Shape(String name, String color, double area, double perimeter){
+        this.name = name;
+        this.color = color;
+        this.area = area;
+        this.perimeter = perimeter;
+    }
 
+    // 3. Methods
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public void printShape(){
+        System.out.println("*************");
+        System.out.println("Name: " + name );
+        System.out.println("Color: " + color);
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+
+    }
 
 }
