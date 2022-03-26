42326d3a96a4f8d03f059d4002bf0a71c5d9795f
chenkunbo <kikichen6677@gmail.com>
2022-03-26 14:44:27 -0700

    Mid-term code

--- Mid-Term/src/com/company/Main.java
+++ Mid-Term/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import Question_1.Student;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Student sample = new Student();
+        Student kunbo = new Student(sample);
+        kunbo.setName("Kunbo");
+        kunbo.setStudentID(007);
+
+        Student gabby = new Student(sample);
+        gabby.setName("Gabby");
+        gabby.setStudentID(8);
+
+        System.out.println("");
+
     }
+
 }
